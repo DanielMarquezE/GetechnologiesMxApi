05c92837b1f1588626c5e4833316d575bb152c5d
dani_ <dani_@DESKTOP-GHR0T5I>
2024-03-25 18:42:12 -0700

    cambios

--- GetechnologiesMxApi/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GetechnologiesMxApi.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
